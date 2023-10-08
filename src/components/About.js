612e09f5a48223f94c18c534dca660925e739fb7
vintha92 <141485349+lilacollier7@users.noreply.github.com>
2023-10-08 09:06:32 -0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,15 +5,8 @@ const About = () => {
     return (
         <section className='about'>
             <h2>Description</h2>
-            <p>Grant Cardone is the author of eight business books, thirteen business programs, and is the CEO of seven privately held companies.  Forbes calls him one of the top social media business influencers in the world.  
-
-Cardone founded and manages a real estate investment firm, Cardone Capital, with $5 Billion assets under management. He also travels the world consulting Fortune 500 companies, small business owners, startups and governments on business expansion.  
-
-One of his enterprises recently hosted The 10X Growth Conference at Miami Marlins Park in Miami, Florida with over 34,000 business people and entrepreneurs in attendance from over fifty countries. 
-
-Mr. Cardone resides in Miami with his wife, Elena Cardone and their two children, Sabrina and Scarlett.   
-
-Visit https://grantcardone.com for his latest blogs, news, tips, training, and where to catch him LIVE!</p>
+            <p>I have the objective of building financial discipline in the masses by simplifying Stock Market Education and Financial concepts. 
+Started my YouTube journey in the year 2019 and since then I have been able to reach out to 4.3 million viewers. Two of my major aims are Localization and Globalization. While the journey of globalization is on through this channel, I have also ventured to the aim of localization through my new channel, in my mother tongue, Marathi.</p>
         </section>
     );
 }
